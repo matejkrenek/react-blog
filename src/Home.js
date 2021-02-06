eda09a3b977c319a0b6ab3a70ed89836a65e1733
matejkrenek <mate23.krenek@gmail.com>
2021-02-06 16:05:32 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import BlogList from './blogList';
+import useFetch from './useFetch';
+
+const Home = () => {
+    const { data: blogs, isLoading, error } = useFetch('http://localhost:8000/blogs');
+
+
+    return ( 
+        <div className="home">
+            { isLoading && <div className="donutSpinner"></div> }
+            { error && <p>{error}</p> }
+            { blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
+        </div>
+    );
+}
+ 
+export default Home;
